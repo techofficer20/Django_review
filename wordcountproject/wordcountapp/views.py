788268f3ed4830af03da5c625241fdcc5e394d41
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-04 23:22:24 +0900

    2-3. wordcount 실습 part 2

--- wordcountproject/wordcountapp/views.py
+++ wordcountproject/wordcountapp/views.py
@@ -7,3 +7,14 @@ def home(request):
 
 def about(request):
     return render(request, "about.html")
+
+def result(request):
+    text = request.GET['fulltext'] # textarea name을 fulltext로 정했다.
+    words = text.split() # 공백을 기준으로 나눈다.
+    word_dictionary = {} # 빈 딕셔너리
+    for word in words:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+    return render(request, "result.html", {"full" : text, 'total' : len(words), 'dictionary' : word_dictionary.items()})
--- wordcountproject/wordcountproject/urls.py
+++ wordcountproject/wordcountproject/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', wordcountapp.views.home, name = "home"),
     path('about/', wordcountapp.views.about, name = "about"),
+    path('result/', wordcountapp.views.result, name = "result"),
 ]
