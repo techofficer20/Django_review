ffee2f5b370bb946a37914052ee6138c644058a2
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-04 23:55:34 +0900

    3-2. model&admin 실습

---
+++ blogproject/blogapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+# 이 부분은 admin 사이트에 보여지는 것임. (단, admin.py와 연결)
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
