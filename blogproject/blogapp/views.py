1af9494347930c1232bdb3b97ccdd01c0ed9714b
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-05 15:16:26 +0900

    5-4. Pagination 실습

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog # models.py에 있는 Blog 객체를 받아와야 함.
 # Create your views here.
 
 def home(request):
     blogs = Blog.objects 
+    blog_list = Blog.objects.all() # 블로그 모든 글들을 대상으로 
+    paginator = Paginator(blog_list, 3) # 블로그 객체 세 개를 한 페이지로 자르기
+    page = request.GET.get('page') # request된 페이지가 뭔지를 알아내고
+    posts = paginator.get_page(page) # request된 페이지를 얻어온 뒤 return 해 준다.
     # 쿼리셋: Blog.objects와 같은 것들..
     # 메소드: 쿼리셋을 좀 더 유용하게 쓸 수 있도록 해주는 것
     # 쿼리셋과 메소드의 형식
     # 모델.쿼리셋(objects).메소드
     # blogs = Blog.objects
     # blogs.all() = Blog.objects.all()
-    return render(request, "home.html", {'blogs' : blogs})
+    return render(request, "home.html", {'blogs' : blogs, 'posts' : posts})
 
 def detail(request, blog_id): # request만으로는 정보 부족. 몇 번 객체를 다룰 것인지 정보 필요.
     blog_detail = get_object_or_404(Blog, pk = blog_id)
