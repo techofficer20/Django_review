5e5daf5d511ed674ad73073158c442e60ae0bf6d
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-05 14:22:37 +0900

    5-2. 로그인, 회원가입 실습

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blogapp.apps.BlogappConfig',
     'portfolioapp.apps.PortfolioappConfig',
+    'accountsapp.apps.AccountsappConfig',
 ]
 
 MIDDLEWARE = [
--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 import blogapp.views
 import portfolioapp.views
+import accountsapp.views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
@@ -24,5 +25,6 @@ urlpatterns = [
     path('', blogapp.views.home, name = "home"),
     path('blog/', include('blogapp.urls')),
     path('portfolio/', portfolioapp.views.portfolio, name = "portfolio"),
+    path('account/', include('accountsapp.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
